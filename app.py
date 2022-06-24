fa5511f975eeb129c955b2749515c14e687d48e2
aminul007bd <gem007bd@gmail.com>
2022-06-24 15:56:37 -0400

    clean up

--- app.py
+++ app.py
@@ -6,40 +6,30 @@ import requests
 
 app = Flask(__name__)
 
+# api key
 ACCESS_KEY = 'V6m3vwqx7XXBcA7Mxfvfxnkc291IhIKgJwYrZcqe'
+# api url for rate conversion
 url = str.__add__('https://api.currencyapi.com/v3/latest?apikey=', ACCESS_KEY) 
-
-
-@app.route("/hello")
-def hello() -> Response:
-    """Simple example of an API endpoint"""
-    return jsonify({"message": "ohai"})
-
-
-@app.route("/hello/<string:name>")
-def hello_name(name: str) -> Response:
-    """Simple example using an URL parameter"""
-    return jsonify({"message": "ohai {}".format(name)})
 
 
 # Latest Exchange Rates for USD,GBP,EUR
 @app.route("/latest")
 def latest_rates() -> Response:
-    latest_rates = url + '&currencies=USD,GBP,EUR'
-    response = requests.get(latest_rates)
+    latest_rates = str.__add__(url ,'&currencies=USD,GBP,EUR')
+    data = requests.get(latest_rates).json()
 
 
-    return jsonify({"message": format(response.json())})
+    return jsonify({"Latest Exchange Rates In (USD, GBP, EUR)": format(data)})
 
 
 # Convert currency depends on base currency and round up to decimal position two
-@app.route("/convert/<string:base_curr>/<string:curr>")
-def convert_currency(base_curr: str, curr: str) -> Response:
-    convert_rates = url + '&base_currency='+ base_curr + '&currencies=' + curr
+@app.route("/convert/<string:base_curr>/<string:converted_curr>")
+def convert_currency(base_curr: str, converted_curr: str) -> Response:
+    convert_rates = url + '&base_currency=' + base_curr + '&currencies=' + converted_curr
   
     data = requests.get(convert_rates).json()
-    curr_value = data["data"][curr]["value"]
+    curr_value = data["data"][converted_curr]["value"]
     curr_value = round(curr_value, 2)
 
-    return jsonify({"message": format(data)})
+    return jsonify({"Converted Rate ": format(data)})
 
