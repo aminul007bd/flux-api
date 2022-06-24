ea4860017408a6e74677afc4b11fe5dab6e6c413
aminul007bd <gem007bd@gmail.com>
2022-06-24 14:12:30 -0400

    latest currency and convert currency

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, jsonify, Response
+
+from flask import Flask, jsonify, Response, Request
+import requests
+
 
 
 app = Flask(__name__)
+
+ACCESS_KEY = 'V6m3vwqx7XXBcA7Mxfvfxnkc291IhIKgJwYrZcqe'
+url = str.__add__('https://api.currencyapi.com/v3/latest?apikey=', ACCESS_KEY) 
 
 
 @app.route("/hello")
@@ -14,3 +20,26 @@ def hello() -> Response:
 def hello_name(name: str) -> Response:
     """Simple example using an URL parameter"""
     return jsonify({"message": "ohai {}".format(name)})
+
+
+# Latest Exchange Rates for USD,GBP,EUR
+@app.route("/latest")
+def latest_rates() -> Response:
+    latest_rates = url + '&currencies=USD,GBP,EUR'
+    response = requests.get(latest_rates)
+
+
+    return jsonify({"message": format(response.json())})
+
+
+# Convert currency depends on base currency and round up to decimal position two
+@app.route("/convert/<string:base_curr>/<string:curr>")
+def convert_currency(base_curr: str, curr: str) -> Response:
+    convert_rates = url + '&base_currency='+ base_curr + '&currencies=' + curr
+  
+    data = requests.get(convert_rates).json()
+    curr_value = data["data"][curr]["value"]
+    curr_value = round(curr_value, 2)
+
+    return jsonify({"message": format(data)})
+
